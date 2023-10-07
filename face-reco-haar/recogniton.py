779ca007802df3d83440329014bfbcb40f819ab1
AadarshaTimilsina <49027210+AadarshaTimilsina@users.noreply.github.com>
2023-10-07 13:10:11 +0545

    Update recogniton.py

--- face-reco-haar/recogniton.py
+++ face-reco-haar/recogniton.py
@@ -1,41 +1,64 @@
 import cv2
-from sklearn.neighbors import KNeighborsClassifier
-import pickle
 import numpy as np
+from sklearn.neighbors import KNeighborsClassifier  # Import the KNeighborsClassifier
+import pickle
 
-facecascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+# Initialize the face cascade classifier
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
-with open('data/faces.pkl', 'rb') as w:
-    faces = pickle.load(w)
+# Load the known faces and labels
+with open('data/faces.pkl', 'rb') as file:
+    known_faces = pickle.load(file)
 
 with open('data/names.pkl', 'rb') as file:
     labels = pickle.load(file)
 
-camera = cv2.VideoCapture(0)
-
-print('Shape of Faces matrix --> ', faces.shape)
+# Create and train the KNN classifier
 knn = KNeighborsClassifier(n_neighbors=4)
-knn.fit(faces,labels)
+knn.fit(known_faces, labels)
+
+# Define a similarity threshold
+threshold = 0.8  # Adjust this threshold as needed
+
+# Initialize the camera
+camera = cv2.VideoCapture(0)
 
 while True:
     ret, frame = camera.read()
-    if ret == True:
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        face_coordinates = facecascade.detectMultiScale(gray, 1.3, 5)
 
-        for (a, b, w, h) in face_coordinates:
-            fc = frame[b:b + h, a:a + w, :]
-            r = cv2.resize(fc, (50, 50)).flatten().reshape(1,-1)
-            text = knn.predict(r)
-            cv2.putText(frame, text[0], (a, b-10), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 0), 2)
-            cv2.rectangle(frame, (a, b), (a + w, b + w), (0, 0, 255), 2)
+    if ret:
+        faces = face_cascade.detectMultiScale(frame, scaleFactor=1.3, minNeighbors=5, minSize=(30, 30))
 
-        cv2.imshow('livetime face recognition', frame)
-        if cv2.waitKey(1) == 27:
+        for (x, y, w, h) in faces:
+            face_roi = frame[y:y + h, x:x + w]
+
+            # Resize the face to match the expected number of features (e.g., 50x50 pixels)
+            face = cv2.resize(face_roi, (50, 50))
+
+            # Flatten the resized face
+            face = face.flatten().reshape(1, -1)
+
+            # Predict the label and calculate similarity score
+            label = knn.predict(face)
+            similarity_score = knn.predict_proba(face).max()
+
+            # Check if the similarity score is below the threshold
+            if similarity_score < threshold:
+                label = "Unknown"
+
+            # Draw the bounding box and label
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            cv2.putText(frame, f"{label} ({similarity_score:.2f})", (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9,
+                        (0, 255, 0), 2)
+
+        cv2.imshow('Face Recognition', frame)
+
+        if cv2.waitKey(1) == 27:  # Press 'Esc' to exit
             break
     else:
-        print("error")
+        print("Error capturing frame")
         break
 
-cv2.destroyAllWindows()
+# Release the camera and close OpenCV windows
 camera.release()
+cv2.destroyAllWindows()
